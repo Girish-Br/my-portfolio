a6b5bb9833081942ba321d03317cf9846fa32609
DIR\Girish BR <girish_br@comcast.com>
2025-09-13 14:16:13 +0530

    updating some changes to make better SEO

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -2,19 +2,23 @@ import React from "react";
 import Typewriter from "typewriter-effect";
 const Type = () => {
   return (
-    <Typewriter
-      options={{
-        strings: [
-          "MERN Stack Developer",
-          "Open Source Contributor",
-          "Software Developer",
-          "Freelancer",
-        ],
-        autoStart: true,
-        loop: true,
-        deleteSpeed: 50,
-      }}
-    />
+    <>
+      This is the official GitHub portfolio of Girish BR, a React.js Frontend Developer.
+      <br/>
+      <Typewriter
+        options={{
+          strings: [
+            "MERN Stack Developer",
+            "Open Source Contributor",
+            "Software Developer",
+            "Freelancer",
+          ],
+          autoStart: true,
+          loop: true,
+          deleteSpeed: 50,
+        }}
+      />
+    </>
   );
 };
 
