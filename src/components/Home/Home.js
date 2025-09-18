615c7ea6b69b81ce5524bc74b3351c178c82ca52
DIR\Girish BR <girish_br@comcast.com>
2025-09-18 11:54:13 +0530

    updating heading

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -30,7 +30,7 @@ const Home = () => {
               </h1>
 
               <h1 className="heading-name">
-                I'M
+                I'm
                 <strong className="main-name"> Girish BR</strong>
               </h1>
 
